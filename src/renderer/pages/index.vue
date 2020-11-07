4bb2d747a00b164f0ed9e0eb2ee3589740b4c507
K-shir0 <kakishiro2190394@gmail.com>
2020-11-07 13:47:08 +0900

    書き込み処理を追加

--- src/renderer/pages/index.vue
+++ src/renderer/pages/index.vue
@@ -3,10 +3,9 @@
     <div class="e-nuxt-content">
       <h1>Hello World!</h1>
       <button v-on:click="openFile()">OpenFile</button>
+      <button v-on:click="saveFile()">saveFile</button>
       <label>
-        <textarea>
-          {{ code }}
-        </textarea>
+        <textarea v-model="code"></textarea>
       </label>
     </div>
   </div>
@@ -15,7 +14,7 @@
 <script>
 import {remote} from "electron";
 import dedent from "dedent";
-import {readFile} from "fs";
+import {readFile, writeFile} from "fs";
 
 /**
  * ## 分割代入
@@ -60,8 +59,27 @@ export default {
     },
 
     /**
+     *
+     * ファイルをセーブするための一連の処理をまとめたもの
+     *
+     */
+    async saveFile() {
+      // ファイル選択ダイアログを開き選択したファイルのパスを取得する処理。
+      const path = await this.selectFile();
+
+      // 書き込みを実行
+      writeFile(path, this.code, (error) => {
+        if (error != null) {
+          alert("save error.");
+        }
+
+
+      })
+    },
+
+    /**
      * ファイルを選択するダイアログを開き、選択されたファイルのパスを取得する処理
-     * @returns {Promise<string>} 選択されたファイルのパスです。
+     * @returns {Promise<string>} 選択されたファイルのパス。
      *
      * ## async awaitとは
      * 非同期のものはfunction名の前にasyncを修飾し、
